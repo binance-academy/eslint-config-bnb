e979c070ed96c14297c319e817025effeb4ed1dd
Brykou <11095630+Brykou@users.noreply.github.com>
2020-07-19 18:46:07 +0900

    Create the eslint config based on Airbnb config

---
+++ README.md
@@ -0,0 +1,11 @@
+# eslint-config-bnb
+
+This package provide Binance Academy's eslint config. It uses [Airbnb's eslint config](https://www.npmjs.com/package/eslint-config-airbnb) and [Prettier config](https://github.com/prettier/eslint-config-prettier)
+
+## Usage
+
+```
+npx install-peerdeps --dev eslint-config-airbnb
+```
+
+Add `"extends": "eslint-config-bnb"` to your eslint config file.
---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  parser: "babel-eslint",
+  parserOptions: {
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    es6: true,
+    browser: true,
+  },
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  rules: {
+    "react/forbid-prop-types": [1, { forbid: ["any"] }],
+    "react/prop-types": ["error", { ignore: ["children"] }],
+    "react/jsx-filename-extension": 0,
+    "react/no-multi-comp": 0,
+    "react/jsx-props-no-spreading": 0,
+  },
+};
